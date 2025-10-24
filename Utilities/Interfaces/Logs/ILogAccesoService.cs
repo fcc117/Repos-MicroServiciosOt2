41d0571aa538787d95b55dce35d975fbf372af07
Fidel Cayetano <fieldgar29@gmail.com>
2025-10-24 16:17:41 -0600

    Añade la interfaz ILogAccesoService y actualiza el proyecto Utilities.

---
+++ Utilities/Interfaces/Logs/ILogAccesoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.Entities.Logs;
+
+namespace Utilities.Interfaces.Logs
+{
+    public interface ILogAccesoService
+    {
+        Task LogAccesoAsync(EntULogAcceso log);
+    }
+}
