b4ba79fae3519249844b8465d53a757a090de102
Fidel Cayetano <fieldgar29@gmail.com>
2025-10-24 16:34:35 -0600

    ent log acceso

---
+++ Utilities/Entities/Logs/EntULogAcceso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Entities.Logs
+{
+    public class EntULogAcceso
+    {
+        public string fcLlaveMaestra { get; set; }
+        public int fnTipoSession { get; set; }
+        public string fcIpMachine { get; set; }
+        public string fcTicketSession { get; set; }
+        public int fnOpcion { get; set; }
+    }
+}
